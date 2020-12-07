8b167214883620e7ae3ee0319e6be526391f116f
Abdurrahman Syzdykov <syzdykovabdurrahman@gmail.com>
2020-12-07 14:29:29 +0600

    created a header component

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+export const Header = () => {
+    return (
+        <header className="header">
+            <h1 className="header__title">To-Do App</h1>
+        </header>
+    )
+}
