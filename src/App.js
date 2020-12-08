cbf79117992158d55dab2d259c4c7d9ae7be8074
Abdurrahman Syzdykov <syzdykovabdurrahman@gmail.com>
2020-12-08 17:28:57 +0600

    created a store context

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
-import React from 'react'
+import React, { useContext, useReducer } from 'react'
 import { Header } from './components/Header';
 import { AddTodo } from './components/AddTodo';
 import { TodoList } from './components/TodoList';
 import { Completed } from './components/Completed';
+import { Store } from './context/Store';
+import { reducer } from './context/reducer';
 
 function App() {
+    const store = useContext(Store)
+    const [state, dispatch] = useReducer(reducer, store)
+
     return (
-        <div>
-            <Header />
-            <div className="container">
-                <AddTodo />
-                <TodoList />
-                <Completed />
+        <Store.Provider value={ { state, dispatch } }>
+            <div>
+                <Header/>
+                <div className="container">
+                    <AddTodo/>
+                    <TodoList/>
+                    <Completed/>
+                </div>
             </div>
-        </div>
+        </Store.Provider>
     );
 }
 
