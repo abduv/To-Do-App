a74eae1b282ebb2638114a2204fd85d4080528c6
Abdurrahman Syzdykov <syzdykovabdurrahman@gmail.com>
2020-12-08 18:14:36 +0600

    implementation of logic in the AddTodo component

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,18 +1,41 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import { Card } from './Card';
+import { Store } from '../context/Store';
+import { ADD_TODO } from '../context/constants';
 
 export const AddTodo = () => {
+    const { state, dispatch } = useContext(Store)
+    const [title, setTitle] = useState('')
+
+    const handleAddTodo = e => {
+        e.preventDefault()
+        if (title) {
+            const newTodo = {
+                title,
+                id: state.todos.length + 1,
+                completed: false
+            }
+            dispatch({ type: ADD_TODO, payload: newTodo })
+            setTitle('')
+        }
+    }
+
     return (
         <>
             <Card title="Add Todo">
                 <form className="addTodo__form">
                     <div className="addTodo__input">
-                        <input type="text" required />
-                        <span className="bar"></span>
+                        <input
+                            type="text"
+                            onChange={ e => setTitle(e.target.value) }
+                            value={title}
+                            required
+                        />
+                        <span className="bar"/>
                         <label>What do you want to do?</label>
                     </div>
                     <div className="addTodo__button">
-                        <button>+</button>
+                        <button type="submit" onClick={ handleAddTodo }>+</button>
                     </div>
                 </form>
             </Card>
--- src/context/reducer.js
+++ src/context/reducer.js
@@ -1,5 +1,12 @@
+import { ADD_TODO } from './constants';
+
 export function reducer(state, action) {
     switch (action.type) {
+        case ADD_TODO:
+            return {
+                ...state,
+                todos: state.todos.concat(...[action.payload])
+            }
         default: return state
     }
 }
